cbcc512158317f8c9f36871630bc9c5efabc4bdc
Cory Finger <fingerco@ccs.neu.edu>
2014-12-24 02:50:38 -0500

    Basic fetching of Registers

--- OSXMemory.rb
+++ OSXMemory.rb
@@ -103,16 +103,16 @@ EOM
   	state = FFI::MemoryPointer.new ThreadState
   	count = FFI::MemoryPointer.new(:int, 1).write_uint ThreadState::COUNT
 
-		r = Libc.thread_get_state(thread, X86_THREAD_STATE64, state, count)
-		abort("THREAD STATE ERROR: #{r}") if r != 0
-		ThreadState.new state
+    r = Libc.thread_get_state(thread, X86_THREAD_STATE64, state, count)
+    abort("THREAD STATE ERROR: #{r}") if r != 0
+    ThreadState.new state
   end
 
   def self.task_for_pid(pid)
-		port = FFI::MemoryPointer.new :uint, 1
-		r = Libc.task_for_pid(Libc.mach_task_self, pid, port)
-		abort("TASK FOR PID ERROR: #{r}") if r != 0
-		port.read_uint
+    port = FFI::MemoryPointer.new :uint, 1
+    r = Libc.task_for_pid(Libc.mach_task_self, pid, port)
+    abort("TASK FOR PID ERROR: #{r}") if r != 0
+    port.read_uint
   end
 
   def self.threads_for_task(task)
