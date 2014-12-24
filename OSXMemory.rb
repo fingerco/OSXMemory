279e4b7c6bda41c66e3d3dd2e09043d7ade9b282
Cory Finger <fingerco@ccs.neu.edu>
2014-12-24 01:44:54 -0500

    Remove stock code

--- OSXMemory.rb
+++ OSXMemory.rb
@@ -2,14 +2,5 @@
 require 'FFI'
 
 class OSXMemory
-
-  module Libc
-    extend FFI::Library
-    ffi_lib FFI::Library::LIBC
-    attach_function :thread_get_state, [:thread_act_t, :thread_state_flavor_t, :pointer, :pointer], :kern_return_t
-    attach_function :thread_set_state, [:thread_act_t, :thread_state_flavor_t, :pointer, :mach_msg_type_number_t], :kern_return_t
-  end
-
-  
 	
 end
